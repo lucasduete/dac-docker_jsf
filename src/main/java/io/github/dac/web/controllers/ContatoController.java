81c1dd6a08f64f35218ed3509055032075819007
Lucas Duete <lm.duete@gmail.com>
2018-08-05 20:11:30 -0300

    Implementando Login e Logout, adicionando página de login, adicionando botão de logout, redirecionando login como welcome page.

--- src/main/java/io/github/dac/web/controllers/ContatoController.java
+++ src/main/java/io/github/dac/web/controllers/ContatoController.java
@@ -3,68 +3,147 @@ package io.github.dac.web.controllers;
 import io.github.dac.models.Contato;
 import io.github.dac.services.ContatoService;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
 @Named
 @SessionScoped
 public class ContatoController implements Serializable {
-
+//as
+    //as
+    //as
     private String busca;
     private String inicial;
+    private String username;
+    private String password;
+
     private Contato contato;
-    private List<Contato> contatos;
+
     private ContatoService service;
+
+    private List<Contato> contatos;
     private List<Contato> contatosLetra;
-    private boolean modoEditando = false;
+
+    private boolean loggado;
+    private boolean modoEditando;
 
     @PostConstruct
     public void init() {
         this.contato = new Contato();
+
+        this.service = new ContatoService();
+
         this.contatos = new ArrayList<>();
         this.contatosLetra = new ArrayList<>();
-        this.service = new ContatoService();
+
+        this.loggado = false;
+        this.modoEditando = false;
     }
 
     public String salvar() {
+        if (loggado) {
+            this.service.salvar(this.contato);
+            this.contato = new Contato();
 
-        this.service.salvar(this.contato);
-        this.contato = new Contato();
+            return "";
+        } else {
 
-        return "";
+            return "login.xhtml";
+        }
+
     }
 
     public String remover(Contato contato) {
-        this.service.remover(contato);
+        if(loggado) {
 
-        return "";
+            this.service.remover(contato);
+            return "";
+        } else {
+
+            return "index.xhtml";
+        }
     }
 
     public String atualizar() {
-        if(this.service.atualizar(contato)) {
-            this.modoEditando = false;
-            this.contato = new Contato();
-        }
+        if (loggado) {
+            if(this.service.atualizar(contato)) {
+                this.modoEditando = false;
+                this.contato = new Contato();
+            }
 
-        return "";
+            return "";
+        } else {
+
+            return "index.xhtml";
+        }
     }
 
     public List<Contato> listarTodos() {
-        return service.listar();
+        if (loggado) {
+
+            return service.listar();
+        } else {
+
+            return Collections.emptyList();
+        }
     }
 
     public List<Contato> listarPorNome() {
-        contatos = this.service.recuperarByNome(busca);
-        return contatos;
+        if (loggado) {
+
+            contatos = this.service.recuperarByNome(busca);
+            return contatos;
+        } else {
+
+            return Collections.emptyList();
+        }
     }
 
     public List<Contato> listarPorIncial() {
-        contatosLetra = this.service.listarPorLetra(inicial);
-        return contatosLetra;
+        if (loggado) {
+
+            contatosLetra = this.service.listarPorLetra(inicial);
+            return contatosLetra;
+        } else {
+
+            return Collections.emptyList();
+        }
+
+    }
+
+    public String login() {
+        if (this.username.equals("admin") && this.password.equals("admin")) {
+
+            this.loggado = true;
+            return "index.xhtml";
+        } else {
+
+            this.loggado = false;
+            return "login.xhtml";
+        }
+    }
+
+    public void logout() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        HttpSession session = (HttpSession) externalContext.getSession(true);
+        session.invalidate();
+
+        try {
+            externalContext.redirect("login.xhtml");
+        } catch (IOException ex) {
+            Logger.getLogger(ContatoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
@@ -115,5 +194,21 @@ public class ContatoController implements Serializable {
 
     public void setContatosLetra(List<Contato> contatosLetra) {
         this.contatosLetra = contatosLetra;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
